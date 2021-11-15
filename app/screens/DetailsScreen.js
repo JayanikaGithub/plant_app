171e7961907b1b69ce6eca931a5173f5665f6d68
JayanikaGithub <AR96588@fhss.sjp.ac.lk>
2021-11-15 23:44:53 +0530

    create details screen

---
+++ app/screens/DetailsScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View } from "react-native";
+
+const DetailsScreen = () => {
+    return (
+        <View>
+            <Text>DETAILSSCREEN</Text>
+        </View>
+    );
+};
+
+export default DetailsScreen;
