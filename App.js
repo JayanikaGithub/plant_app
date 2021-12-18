fea7c09cde31dcbbbeede8a25ddd7d10337a09b1
JayanikaGithub <AR96588@fhss.sjp.ac.lk>
2021-12-18 23:10:11 +0530

    create signup screen and other necessary files and folders

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import OnBoardScreen from './app/screens/OnBoardScreen';
 import SignInScreen from './app/screens/SignInScreen';
+import SignUpScreen from './app/screens/SignUpScreen';
 import registration from './app/screens/registration';
 import HomeScreen from '../final_project/app/screens/HomeScreens';
 import DetailsSceeen from '../final_project/app/screens/DetailsScreen';
@@ -19,6 +20,7 @@ const App = () => {
       <Stack.Navigator screenOptions={{header: () => null}}>
         <Stack.Screen name="BoardScreen" component={OnBoardScreen} />
         <Stack.Screen name="Signinscreen" component={SignInScreen} />
+        <Stack.Screen name="SignUpscreen" component={SignUpScreen} />
         <Stack.Screen name="reg" component={registration} />
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Details" component={DetailsSceeen} />
